b533c5fea06a05631bc6362700be821aecf6cbb7
jordnkr <jordnkr408@gmail.com>
2023-05-14 20:44:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,27 +7,36 @@ const outcomes = ["1", "2", "3", "4", "Bird/Dog", "Spilled Bucket"];
 
 // Draw the spinner
 function drawSpinner(angle) {
-    const radius = canvas.width / 2;
+    const radius = (canvas.width - 20) / 2; // Reduce the radius by 10 to account for the padding
     const sections = outcomes.length;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     for (let i = 0; i < sections; i++) {
         ctx.beginPath();
-        ctx.moveTo(radius, radius);
-        ctx.arc(radius, radius, radius, (angle + (i * 2 * Math.PI) / sections), (angle + ((i + 1) * 2 * Math.PI) / sections), false);
+        ctx.moveTo(radius + 10, radius + 10); // Add 10 to account for the padding
+        ctx.arc(radius + 10, radius + 10, radius, (angle + (i * 2 * Math.PI) / sections), (angle + ((i + 1) * 2 * Math.PI) / sections), false);
         ctx.closePath();
         ctx.fillStyle = i % 2 === 0 ? "#000" : "#ff0000";
         ctx.fill();
 
         // Draw outcome text
         ctx.save();
-        ctx.translate(radius, radius);
+        ctx.translate(radius + 10, radius + 10); // Add 10 to account for the padding
         ctx.rotate(angle + ((i + 0.5) * 2 * Math.PI) / sections);
         ctx.fillStyle = "#fff";
         ctx.textAlign = "center";
         ctx.fillText(outcomes[i], radius / 2, 0);
         ctx.restore();
     }
+
+    // Draw the spinner post
+    ctx.beginPath();
+    ctx.moveTo(radius + 5, 0);
+    ctx.lineTo(radius + 15, 0);
+    ctx.lineTo(radius + 10, 20);
+    ctx.closePath();
+    ctx.fillStyle = "#000";
+    ctx.fill();
 }
 
 // Spin the spinner
